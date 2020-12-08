b5fced9f0151247aef2bf0e7569b0c81a992997e
Seonggyu Park <envpsg@gmail.com>
2020-12-08 17:16:36 -0600

    updated short cv

--- short_cv/render_cv.R
+++ short_cv/render_cv.R
@@ -5,10 +5,10 @@
 # loaded and load the cached version in the .Rmd instead of re-fetching it twice
 # for the HTML and PDF rendering. This exercise is left to the reader.
 setwd("D:/spark-brc_gits/spark-brc.github.io/short_cv")
-# Knit the HTML version
-rmarkdown::render("cv.Rmd",
-                  params = list(pdf_mode = FALSE),
-                  output_file = "index.html")
+# # Knit the HTML version
+# rmarkdown::render("cv.Rmd",
+#                   params = list(pdf_mode = FALSE),
+#                   output_file = "for_pdf.html")
 
 # # Knit the PDF version to temporary html location
 # tmp_html_cv_loc <- fs::file_temp(ext = ".html")
@@ -16,6 +16,6 @@ rmarkdown::render("cv.Rmd",
 #                   params = list(pdf_mode = TRUE),
 #                   output_file = tmp_html_cv_loc)
 # 
-# # Convert to PDF using Pagedown
-# pagedown::chrome_print(input = 'for_pdf.html',
-#                        output = "shortIntro_SeonggyPark.pdf")
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = 'for_pdf.html',
+                       output = "Seonggyu Park's Projects & Publications.pdf")
