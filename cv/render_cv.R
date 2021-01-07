a8d774a74cfe64b945e37579ef1370614046b273
Seonggyu Park <envpsg@gmail.com>
2021-01-07 16:16:54 -0600

    updated CV

--- cv/render_cv.R
+++ cv/render_cv.R
@@ -6,9 +6,13 @@
 # for the HTML and PDF rendering. This exercise is left to the reader.
 setwd("D:/spark-brc_gits/spark-brc.github.io/cv")
 # Knit the HTML version
-rmarkdown::render("cv.Rmd",
-                  params = list(pdf_mode = FALSE),
-                  output_file = "index.html")
+# rmarkdown::render("cv.Rmd",
+#                   params = list(pdf_mode = FALSE),
+#                   output_file = "index.html")
+
+# rmarkdown::render("cv.Rmd",
+#                   params = list(pdf_mode = FALSE),
+#                   output_file = "for_pdf.html")
 
 # # Knit the PDF version to temporary html location
 # tmp_html_cv_loc <- fs::file_temp(ext = ".html")
@@ -16,6 +20,6 @@ rmarkdown::render("cv.Rmd",
 #                   params = list(pdf_mode = TRUE),
 #                   output_file = tmp_html_cv_loc)
 # 
-#Convert to PDF using Pagedown
-# pagedown::chrome_print(input = 'for_pdf.html',
-#                        output = "cv.pdf")
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = 'for_pdf.html',
+                       output = "Seonggyu Park's CV.pdf")
